fefc15f3f4fcfaf2b3aa677639efd71c851b88d5
Mohosin <mohosinali075@gmail.com>
2024-07-08 02:23:57 +0600

    carts data manage

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fudiykq.mongodb.net/?appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -32,10 +32,35 @@ async function run() {
 
 
     const menuCollection = client.db('bistro-boss-recap').collection('menuCollection');
+    const reviewsCollection = client.db('bistro-boss-recap').collection('reviews');
+    const cartCollection = client.db('bistro-boss-recap').collection('carts');
 
     app.get('/menu', async(req, res)=>{
         const result = await menuCollection.find().toArray()
         res.send(result)
+    })
+
+    // carts data load
+
+    app.get('/carts', async(req, res)=>{
+      const email= req.query.email
+      const query = {email : email}
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    })
+    app.post('/carts', async(req, res)=>{
+      const carts = req.body
+      const result = await cartCollection.insertOne(carts);
+      res.send(result)
+    })
+
+    // delete cart data
+
+    app.delete('/carts/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id) }
+      const result = await cartCollection.deleteOne(query)
+      res.send(result)
     })
 
 
